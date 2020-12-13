8b729bc8d8ea921b81ae60465670b4c09bcec669
Candice Canoso <candice@Candices-MacBook.local>
2020-12-13 16:38:48 +0800

    1.3 - Added filter function

--- static/js/checkin-pie.js
+++ static/js/checkin-pie.js
@@ -28,7 +28,7 @@ class CndcePie{
             this.arc = d3.arc()
                 .innerRadius(50)
                 .outerRadius(Math.min(this.width, this.height) / 2 - 1)
-                .cornerRadius(15)
+                .cornerRadius(20)
 
             const radius = Math.min(this.width, this.height) / 2 * 0.8;
             this.arcLabel = d3.arc()
@@ -39,8 +39,6 @@ class CndcePie{
                 .attr('stroke', 'white')
                 .attr('class', 'cndce-pie-g')
 
-            this.svg.append('g')
-                .attr('class', 'cndce-pie-labels-g')
 
             this.updateD3()
 
@@ -50,6 +48,8 @@ class CndcePie{
     updateD3(){
         const __data = CNDCE.groupedBy.tag;
 
+        console.log(CNDCE.groupedBy.tag);
+
         // Convert to array
         const data = Object.keys(__data).map((key)=>__data[key])
 
@@ -57,35 +57,64 @@ class CndcePie{
 
         const arcsG = this.svg
             .select('.cndce-pie-g')
-            .selectAll('path')
+            .selectAll('g')
+            .data(arcs)
 
-        const pathG = arcsG.data(arcs)
+        const pathG = arcsG
             .enter()
             .append('g')
             .attr('class', 'cndce-pie-arcs')
             .attr('fill', (d, i) => d3.schemePaired[i])
 
         pathG.append('path')
+            .attr('class', 'cndce-pie-arch-path')
+
+        // Update Values
+        // TODO: Clean this up. Redudant selection but I'm out of time so have no choice
+        this.svg.selectAll('.cndce-pie-arch-path')
+            .data(arcs)
+            .transition()
             .attr('d', this.arc)
-            .append('title')
             .text(d => d.data.tag.name)
+
 
         pathG.append('text')
             .attr('class', 'cndce-pie-label')
-            .attr('transform', d => `translate(${this.arc.centroid(d)})`)
             .call(text => (
                 text.append('tspan')
+                    .attr('class', 'cndce-pie-label--title')
                     .attr('y', '-0.4em')
                     .attr('font-weight', 'bold')
                     .text(d => d.data.tag.name)
             ))
-            .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
-                .attr("x", 0)
-                .attr("y", "1rem")
-                .attr("fill-opacity", 0.7)
-                .text(d => `Total: ${d.data.hours.toFixed(2)} hours`));
+            .call(text => 
+                text.filter(d => (d.endAngle - d.startAngle) > 0.25)
+                    .append("tspan")
+                    .attr('class', 'cndce-pie-label--desc')
+                    .attr("x", 0)
+                    .attr("y", "1rem")
+                    .attr("fill-opacity", 0.7)
+                    .text(d => `Total: ${d.data.hours.toFixed(2)} hours`
+                )
+            );
 
-        arcsG.exit().remove()
+        // Update labels
+        this.svg.selectAll('.cndce-pie-label')
+            .data(arcs)
+            .transition()
+            .attr('transform', d => `translate(${this.arc.centroid(d)})`)
+            .call(text => (
+                text.select('.cndce-pie-label--title')
+                    .text(d => d.data.tag.name)
+            ))
+            .call(text =>
+                text.filter(d => (d.endAngle - d.startAngle) > 0.25)
+                    .select('.cndce-pie-label--desc')
+                    .text(d => `Total: ${d.data.hours.toFixed(2)} hours`
+                )    
+            )
+
+        arcsG.exit().transition().remove()
 
             
         
@@ -100,5 +129,5 @@ class CndcePie{
 
 
 $(()=>{
-    new CndcePie();
+    CNDCE.cndcePie = new CndcePie();
 })
--- static/js/checkin-table.js
+++ static/js/checkin-table.js
@@ -1,36 +1,29 @@
+// TODO: Convert to class
+
 CNDCE.loadCheckins = new Promise((resolve)=>{
     $(function(){
         const $checkinTable = $('.cndce-checkin-table');
-    
-    
+        const $filterDateRange = $('.filter-date-range input');
+
+        // Init DateRange picker
+        $filterDateRange.daterangepicker({
+            timePicker: true
+        }, __onDateRangeChange)
+
+        
+        // Load data
         $.ajax({
             url: '/api/checkin/table',
             success: function(json){
     
                 CNDCE.checkinData = json;
+                CNDCE.checkinDataVisible = json.checkins;
     
                 $checkinTable.bootstrapTable({
                     data: json.checkins
                 });
 
-                // Group by tag
-                CNDCE.groupedBy = {
-                    tag: CNDCE.groupby(
-                        json.checkins, 
-                        (d)=>d.tag.name, 
-                        (g, d) => ({
-                            hours: g.hours + d.hours,
-                            activity: [
-                                ...Array.isArray(g.activity) ? g.activity : [g.activity],
-                                d.activity
-                            ],
-                            date: [
-                                ...Array.isArray(g.date) ? g.date : [g.date],
-                                d.date
-                            ]
-                        }) 
-                    )
-                }
+                CNDCE.__initGroupedByPresets();
 
                 resolve(json);
             },
@@ -39,9 +32,56 @@ CNDCE.loadCheckins = new Promise((resolve)=>{
                 alert('There was an error in retrieving the data. Check in console for more details or reload the page');
             }
         })
+
+
+        function __onDateRangeChange(start, end, label){
+            const data = CNDCE.checkinData.checkins
+
+            const dateStart = new Date(start._d);
+            const dateEnd = new Date(end._d);
+
+
+            CNDCE.updateCheckinData(data.filter((d, i)=>{
+                const date = new Date(d.date);
+
+                return isDateValid = date >= dateStart && date <= dateEnd;
+            }))
+
+        }
+
+        // Events
+        $('#cndce-reset-button').click(function(){
+            CNDCE.updateCheckinData(CNDCE.checkinData.checkins)
+        })
     
     })
 });
+
+
+CNDCE.updateCheckinData = (newData) => {
+    const $checkinTable = $('.cndce-checkin-table');
+
+    CNDCE.checkinDataVisible = newData;
+
+    CNDCE.checkinData.checkins.forEach((d, i)=>{
+        if(CNDCE.checkinDataVisible.indexOf(d) != -1){
+            $checkinTable.bootstrapTable('showRow', {index: i})
+
+        }else{
+            $checkinTable.bootstrapTable('hideRow', {index: i})
+
+        }
+    })
+
+
+    // Update grouped by presets
+    CNDCE.__initGroupedByPresets();
+
+    if(CNDCE.cndcePie){
+        CNDCE.cndcePie.updateD3();
+        console.log('Updated pie')
+    }
+}
 
 
 CNDCE.groupby = (array, getGroupByKey, getAggregate)=>{
@@ -59,6 +99,28 @@ CNDCE.groupby = (array, getGroupByKey, getAggregate)=>{
             }
         }
     }, {})
+}
+
+
+CNDCE.__initGroupedByPresets = () => {
+    CNDCE.groupedBy = {
+        // Group by tag
+        tag: CNDCE.groupby(
+            CNDCE.checkinDataVisible, 
+            (d)=>d.tag.name, 
+            (g, d) => ({
+                hours: g.hours + d.hours,
+                activity: [
+                    ...Array.isArray(g.activity) ? g.activity : [g.activity],
+                    d.activity
+                ],
+                date: [
+                    ...Array.isArray(g.date) ? g.date : [g.date],
+                    d.date
+                ]
+            }) 
+        )
+    }
 }
 
 
